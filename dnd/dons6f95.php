4c1ac7ad8fe39a63553f551fbfd64e1550ab9f7b
marsmouche <82277279+marsmouche@users.noreply.github.com>
2022-06-14 08:38:41 -0400

    Add files via upload

---
+++ dnd/dons6f95.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+
+<!-- Mirrored from www.aidedd.org/dnd/dons.php?vf=adepte-occulte by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Mar 2022 23:21:08 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="initial-scale=1">
+	<link href="style3860.css?v=1" rel="stylesheet" type="text/css" /> 
+	<style>
+		body			{background:white url("../images/fond-ph.jpg") repeat}
+		.blocCarte h1	{background-color:#C93C0C; color:white}
+	</style>
+	
+<title>Adepte occulte - Don &raquo; Donjons &amp; Dragons - D&amp;D 5e</title><link rel="alternate" href="dons6f95.php?vf=adepte-occulte" hreflang="fr" /><link rel="alternate" href="dons20e9.html?vo=eldritch-adept" hreflang="en" /><link rel="canonical" href="dons6f95.php?vf=adepte-occulte" /></head><body><div class="cols1A"><div class="content"><div class="bloc"><h1>Adepte occulte</h1><div class="trad">[ <a href="dons20e9.html?vo=eldritch-adept">Eldritch Adept</a> ]</div><div class="prerequis">Prérequis : Capacité Incantation ou Magie de pacte</div><p class="resume">Description non disponible (non OGL). Résumé :</p><div class="description">Vous apprenez une manifestation occulte.</div><div class='source'>Tasha´s Cauldron of Everything</div><div class='ref'>&rarr; <strong><a href="../dnd-filters/dons.html">Dons D&amp;D 5e</a></strong></div></div></div>				<script async src="https://www.googletagmanager.com/gtag/js?id=UA-346920-5"></script>
+				<script>
+				  window.dataLayer = window.dataLayer || [];
+				  function gtag(){dataLayer.push(arguments)}
+				  gtag('js', new Date());
+				  gtag('config', 'UA-346920-5');
+				</script>
+
+				<div class="ads">
+					<script async src="../../pagead2.googlesyndication.com/pagead/js/f32f7.txt?client=ca-pub-5355421917378265"
+					  crossorigin="anonymous"></script>
+					<!-- Fiche -->
+					<ins class="adsbygoogle"
+					  style="display:block"
+					  data-ad-client="ca-pub-5355421917378265"
+					  data-ad-slot="8650354676"
+					  data-ad-format="rectangle"
+					  data-full-width-responsive="true"></ins>
+					<script>
+					  (adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</div>
+			</div></body>
+
+<!-- Mirrored from www.aidedd.org/dnd/dons.php?vf=adepte-occulte by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Mar 2022 23:21:08 GMT -->
+</html>
